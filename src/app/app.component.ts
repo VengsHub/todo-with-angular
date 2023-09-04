be219d7bb8900b5a590569614d3e7a9966a0015d
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2023-09-04 15:55:14 +0200

    basic todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoItem, TodoListService } from './todo-list.service';
+import { fromEvent, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NgLetModule } from 'ng-let';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NgIf, NgFor, RouterOutlet, NgLetModule, FormsModule],
+  providers: [TodoListService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-with-angular';
+
+  readonly mousePosition$ = fromEvent<MouseEvent>(document, 'mousemove').pipe(
+    map(event => ({x: event.clientX, y: event.clientY}))
+  );
+  cMousePosition = toSignal(this.mousePosition$, {initialValue: {x: 0, y: 0}});
+
+  sNewTodo = signal('');
+  readonly sMovingTodo = signal<TodoItem | undefined>(undefined);
+  constructor(public readonly todoListService: TodoListService) {
+  }
 }
---
+++ src/app/todo-list.service.ts
@@ -0,0 +1,40 @@
+import { computed, signal } from '@angular/core';
+
+export type TodoItem = {
+  text: string;
+  done: boolean;
+}
+
+export class TodoListService {
+  readonly sTodoList = signal<TodoItem[]>([
+    {text: 'Feed the dog.', done: true},
+    {text: 'Feed the cat.', done: false},
+    {text: 'Feed myself.', done: false}
+  ]);
+  readonly cTodoList = computed(() => this.sTodoList());
+
+  addTodo(newTodo: TodoItem) {
+    return this.sTodoList.update(todos => todos.concat(newTodo));
+  }
+
+  updateTodoStatus(todoToUpdate: TodoItem, newStatus: boolean) {
+    return this.sTodoList.update(todos =>
+      todos.map(todo => todo.text === todoToUpdate.text ? {...todoToUpdate, done: newStatus} : todo)
+    );
+  }
+
+  removeTodo(todoToRemove: TodoItem) {
+    return this.sTodoList.update(todos =>
+      todos.filter(todo => todo.text !== todoToRemove.text)
+    );
+  }
+
+  moveTodo(todoToMove: TodoItem, position: number) {
+    return this.sTodoList.update(todos => {
+      const movedTodoIndex = todos.findIndex(todo => todo.text === todoToMove.text);
+      todos.splice(movedTodoIndex, 1);
+      todos.splice(position, 0, todoToMove);
+      return todos;
+    });
+  }
+}
