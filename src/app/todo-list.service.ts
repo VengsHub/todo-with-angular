c53ba7649fc66a0869b95315df46a776d776149b
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2023-09-05 22:49:00 +0200

    add http logic

--- src/app/todo-list.service.ts
+++ src/app/todo-list.service.ts
@@ -1,36 +1,51 @@
-import { computed, signal } from '@angular/core';
+import { Injectable, Signal, signal } from '@angular/core';
+import { TodoListRestService } from './todo-list-rest.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 export type TodoItem = {
   text: string;
   done: boolean;
 }
 
+@Injectable()
 export class TodoListService {
-  readonly sTodoList = signal<TodoItem[]>([
+
+  private readonly sTodoList = signal<TodoItem[]>([
     {text: 'Feed the dog.', done: true},
     {text: 'Feed the cat.', done: false},
     {text: 'Feed myself.', done: false}
   ]);
-  readonly cTodoList = computed(() => this.sTodoList());
+
+  readonly todoList$ = merge(toObservable(this.sTodoList), this.restService.todoList$);
+  readonly cTodoList: Signal<TodoItem[]> = toSignal(this.todoList$, {initialValue: this.sTodoList()});
+
+  constructor(private readonly restService: TodoListRestService) {
+    // Rule: do not subscribe unless absolutely necessary --> if necessary, try to subscribe in constructors only
+    // restService.todoList$.pipe(takeUntilDestroyed()).subscribe(todoList =>
+    //   this.sTodoList.set(todoList)
+    // );
+  }
 
   addTodo(newTodo: TodoItem) {
-    return this.sTodoList.update(todos => todos.concat(newTodo));
+    this.restService.addTodoSubject.next(newTodo);
   }
 
   updateTodoStatus(todoToUpdate: TodoItem, newStatus: boolean) {
-    return this.sTodoList.update(todos =>
-      todos.map(todo => todo.text === todoToUpdate.text ? {...todoToUpdate, done: newStatus} : todo)
+    return this.sTodoList.set(
+      this.cTodoList().map(todo => todo.text === todoToUpdate.text ? {...todoToUpdate, done: newStatus} : todo)
     );
   }
 
   removeTodo(todoToRemove: TodoItem) {
-    return this.sTodoList.update(todos =>
-      todos.filter(todo => todo.text !== todoToRemove.text)
+    return this.sTodoList.set(
+      this.cTodoList().filter(todo => todo.text !== todoToRemove.text)
     );
   }
 
   moveTodo(todoToMove: TodoItem, position: number) {
-    return this.sTodoList.update(todos => {
+    return this.sTodoList.update(() => {
+      const todos = this.cTodoList();
       const movedTodoIndex = todos.findIndex(todo => todo.text === todoToMove.text);
       todos.splice(movedTodoIndex, 1);
       todos.splice(position, 0, todoToMove);
