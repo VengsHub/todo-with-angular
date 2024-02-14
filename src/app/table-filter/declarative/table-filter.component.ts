8460f069412efcb490590d7e7889b27dbefb73e3
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2024-02-14 17:11:00 +0100

    show good and bad rxjs approach

--- src/app/table-filter/declarative/table-filter.component.ts
+++ src/app/table-filter/declarative/table-filter.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { combineLatest, map, of } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, of } from 'rxjs';
 import { AsyncPipe, NgForOf } from '@angular/common';
 import { Item } from '../../drag-and-drop/item.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -17,13 +18,26 @@ import { Item } from '../../drag-and-drop/item.model';
   styleUrls: ['./table-filter.component.scss']
 })
 export class TableFilterComponent {
-  filter = new FormControl('', {nonNullable: true});
-  items$ = of<Item[]>([]);
+  readonly filter = new FormControl('', {nonNullable: true});
 
-  filteredItems$ = combineLatest({
+  // GOOD
+  // state changes defined during declaration -> one way data flow -> functional & safe
+  readonly items$ = of<Item[]>([]);
+  readonly filteredItems$ = combineLatest({
     items: this.items$,
     filter: this.filter.valueChanges
   }).pipe(
       map(({items, filter}) => items.filter(item => item.text.includes(filter)))
   );
+
+  // BAD
+  // state change in subscribe -> risk of two-way data flow -> danger of recursion and messy state
+  itemsS = new BehaviorSubject<Item[]>([]);
+  constructor() {
+    this.filter.valueChanges.pipe(takeUntilDestroyed()).subscribe(filter => {
+      this.itemsS.next(this.itemsS.value.filter(item => item.text.includes(filter)));
+    });
+
+    // this.itemsS.subscribe(() => this.filter.setValue('')); // <-- ERROR
+  }
 }
