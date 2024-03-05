c159df5bcf089277c04b6bc53743ea2fc88bc38b
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2024-03-05 20:18:38 +0100

    improve all todo list rest logic

--- src/app/todo-list-with-rest/todo-list.service.ts
+++ src/app/todo-list-with-rest/todo-list.service.ts
@@ -6,45 +6,63 @@ import { toSignal } from '@angular/core/rxjs-interop';
 @Injectable()
 export class TodoListService {
   private readonly polledTodoList$ = timer(1, 60000).pipe(
-      switchMap(() => this.restService.getTodoList()),
-      shareReplay(1)
+    switchMap(() => this.restService.getTodoList()),
+    shareReplay(1)
   );
 
   readonly addTodoSubject = new Subject<TodoItem>();
   private readonly addTodo$: Observable<TodoItem[]> = this.addTodoSubject.pipe(
-      map(todo => this.cTodolist().concat(todo)),
-      switchMap(todos => this.restService.updateTodoList(todos)),
-      shareReplay(1)
+    map(todo => this.cTodolist().concat(todo)),
+    switchMap(todos => this.restService.updateTodoList(todos)),
+    shareReplay(1)
   );
 
-  readonly moveTodoSubject = new Subject<{movingTodo: TodoItem, index: number}>();
-  // private readonly moveTodo$ = this.moveTodoSubject.pipe(
-  //   switchMap(todo => this.restService.updateTodo(todo)),
-  //   shareReplay(1)
-  // );
+  readonly moveTodoSubject = new Subject<{todo: TodoItem, index: number}>();
+  private readonly moveTodo$: Observable<TodoItem[]> = this.moveTodoSubject.pipe(
+    filter(moving => moving.todo.id !== this.cTodolist()[moving.index].id),
+    switchMap(moving =>
+      this.restService.updateTodoList(this.moveTodoInList(this.cTodolist(), moving.todo, moving.index))
+    ),
+    shareReplay(1)
+  );
 
   readonly updateTodoStatusSubject = new Subject<{todo: TodoItem, status: boolean}>();
-  // private readonly updateTodoStatus$ = this.updateTodoStatusSubject.pipe(
-  //   switchMap(updated => this.restService.updateTodo({...updated.todo, done: updated.status})),
-  //   shareReplay(1)
-  // );
+  private readonly updateTodoStatus$: Observable<TodoItem[]> = this.updateTodoStatusSubject.pipe(
+    switchMap(updated => this.restService.updateTodoList(
+      this.cTodolist().map(todo => todo.id === updated.todo.id ? {...todo, done: updated.status} : todo)
+    )),
+    shareReplay(1)
+  );
 
   readonly removeTodoSubject = new Subject<TodoItem>();
-  // private readonly removeTodo$ = this.removeTodoSubject.pipe(
-  //   switchMap(todo => this.restService.removeTodo(todo)),
-  //   shareReplay(1)
-  // );
+  private readonly removeTodo$: Observable<TodoItem[]> = this.removeTodoSubject.pipe(
+    switchMap(removeTodo => this.restService.updateTodoList(
+      this.cTodolist().filter(todo => todo.id !== removeTodo.id)
+    )),
+    shareReplay(1)
+  );
 
   readonly todoList$ = merge(
     this.polledTodoList$,
-    this.addTodo$
+    this.addTodo$,
+    this.moveTodo$,
+    this.updateTodoStatus$,
+    this.removeTodo$
   ).pipe(
-      filter((list): list is TodoItem[] => !!list),
-      shareReplay(1)
+    filter((list): list is TodoItem[] => !!list),
+    shareReplay(1)
   );
 
-  readonly cTodolist = toSignal(this.todoList$, {initialValue: <TodoItem[]>[]})
+  readonly cTodolist = toSignal(this.todoList$, {initialValue: <TodoItem[]>[]});
 
   constructor(private readonly restService: TodoListRestService) {
+  }
+
+  moveTodoInList(todoList: TodoItem[], todoToMove: TodoItem, position: number) {
+    const movedTodoIndex = todoList.findIndex(todo => todo.text === todoToMove.text);
+    const updatedList = [...todoList]
+    updatedList.splice(movedTodoIndex, 1);
+    updatedList.splice(position, 0, todoToMove);
+    return updatedList;
   }
 }
